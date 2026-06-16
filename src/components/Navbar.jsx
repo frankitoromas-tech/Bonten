0bfcf30c692dad1c700e5cdddaab1a42768d156f
Frank Vargas <frankitoromas@gmail.com>
2026-06-16 10:43:06 -0500

    Fix: se movió el logo a la carpeta public

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar({ setPage, activePage, darkMode, toggleDarkMode }
                     <h1 className="logo">BONTEN</h1>
                     <span className="badge-ng">NG</span>
                 </div>
-<img src="/BONTEN_LOGO0.jpg" alt="Logo Bonten" />
+                <img src="/BONTEN_LOG0.jpg" alt="Logo Bonten" className="logo-img" />
             </a>
             <nav className="top-nav">
                 <a href="#" onClick={(e) => { e.preventDefault(); setPage('inicio'); }} className={`nav-link ${activePage === 'inicio' ? 'active' : ''}`}>Inicio</a>
